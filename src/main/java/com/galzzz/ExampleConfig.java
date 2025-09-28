17d2101cda70c150e967bd73d9c8f052f2b52def
GaLzZy <gab.aucoin8383@outlook.com>
2025-09-28 11:17:15 -0400

    first commit

--- src/main/java/com/galzzz/ExampleConfig.java
+++ src/main/java/com/galzzz/ExampleConfig.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.galzzz;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
--- src/test/java/com/galzzz/PluginLauncher.java
+++ src/test/java/com/galzzz/PluginLauncher.java
@@ -1,9 +1,9 @@
-package com.example;
+package com.galzzz;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class PluginLauncher
 {
 	public static void main(String[] args) throws Exception
 	{
